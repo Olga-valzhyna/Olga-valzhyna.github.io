ee313ba90987a9a0f90fed83ed15b256960c44f3
Olga <virginia@tut.by>
2021-03-22 16:23:00 +0100

    w

---
+++ webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {6:-1,5:-1,10:-1,0:-1,9:-1,4:-1,1:-1,3:-1,12:-1,11:-1,2:-1,7:-1,8:-1};
+});
