ec656fd7c8d465c4f55d81d3553f0c365a9bc348
Olga <virginia@tut.by>
2021-03-23 18:53:45 +0100

    f

---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {12:-1,11:-1,4:-1,7:-1,3:-1,8:-1,5:-1,6:-1,2:-1,9:-1,10:-1,0:-1,1:-1};
+});
